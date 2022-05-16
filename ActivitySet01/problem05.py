b12ccc9627de74d9c43fa51242a15bfc58dcad86
SanjanaNidigol <sanjananidigol@gmail.com>
2022-05-16 10:19:22 +0530

    score

--- ActivitySet01/problem05.py
+++ ActivitySet01/problem05.py
@@ -1,7 +1,24 @@
 # Functions
 
+score =float(input("Enter Score: "))
+if score > 1.0:
+    print('Error')
+elif score >= 0.9:
+    print('A')
+elif score >= 0.8:
+    print('B')
+elif score >= 0.7:
+    print('C')
+elif score >= 0.6:
+    print('D')
+else: 
+    print('F')
 
-def computepay(h, r):
+
+
+
+
+'''def computepay(h, r):
     pass  # ...
 
 
@@ -9,4 +26,4 @@ hrs = float(input("Enter hours? "))
 rte = float(input("Enter rate per hour? "))
 
 p = computepay(hrs, rte)
-print("Pay", p)
+print("Pay", p)'''
