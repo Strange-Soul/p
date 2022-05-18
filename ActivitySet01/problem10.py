d637e83729196fe6900467c46e470f2563961559
SanjanaNidigol <95744225+SanjanaNidigol@users.noreply.github.com>
2022-05-18 10:25:37 +0000

    Pay

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,9 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+
+hrs = input("Enter Hours:")
+hours=float(hrs)
+rate=float(input("Enter rate :"))
+Pay=hours*rate 
+print("Pay:", Pay)
