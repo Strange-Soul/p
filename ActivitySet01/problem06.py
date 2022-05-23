af445cdc6cc0c2f9854b223d1e9abd01699e5f40
SanjanaNidigol <95744225+SanjanaNidigol@users.noreply.github.com>
2022-05-23 04:47:16 +0000

    trynexcept

--- ActivitySet01/problem06.py
+++ ActivitySet01/problem06.py
@@ -1,4 +1,26 @@
-# Loops & Iterators
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num=int(num)
+    except:
+        print("Invalid input")
+        continue 
+    if largest is None or num > largest :
+        largest = num
+    
+    if smallest is None or num < smallest:
+            smallest = num
+print("Maximum is", largest)
+print("Minimum is",smallest)
+
+
+
+
+'''# Loops & Iterators
 
 largest = None
 smallest = None
@@ -13,4 +35,4 @@ while True:
 
     print(num)
 
-print("Maximum", largest)
+print("Maximum", largest)'''
