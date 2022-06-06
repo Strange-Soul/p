f171145ab4d9c95f3adba9e45bb038e6c62d2a20
SanjanaNidigol <95744225+SanjanaNidigol@users.noreply.github.com>
2022-06-06 03:22:03 +0000

    str

--- ActivitySet01/problem07.py
+++ ActivitySet01/problem07.py
@@ -1,3 +1,7 @@
 # Strings
 
 text = "X-DSPAM-Confidence:    0.8475"
+x=text.find(":")
+y=text[x+1:]
+z=float(y)
+print(z)
--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,12 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+a=int(input("enter a num:"))
+b=int(input("enter a num:"))
+c=int(input("enter a num:"))
+if a>b and a>c:
+    print(f"{a} is largest num")
+elif b>c:
+    print(f"{b} is largest num")
+else:
+    print(f"{c} is largest num")
